e9f6ea62395a2235b23708c7fca92c73ca5de267
draco <wecke@neoskop.de>
2020-03-12 14:41:57 +0100

    feat(plugin): add serve

---
+++ modules/plugin/src/serve.spec.ts
@@ -0,0 +1,21 @@
+import { Server } from 'grpc';
+import { ObjectWritableMock } from 'stream-mock';
+import { serve } from './serve';
+
+describe('serve', () => {
+    it('should throw on missing host arg', () => {
+        expect(serve([], () => null!, { stdout: false })).rejects.toThrow(new Error('Missing argument for host RPC'))
+    });
+
+    it('should return server and port', async () => {
+        const stdout = new ObjectWritableMock({ objectMode: true });
+        const { server, port } = await serve(['0.0.0.0:0'], () => ({} as any), { stdout });
+
+        expect(server).toBeInstanceOf(Server);
+        expect(port).toBeGreaterThanOrEqual(1024);
+        expect(port).toBeLessThanOrEqual(49151);
+        expect(stdout.data).toEqual([`${port}\n`]);
+
+        server.forceShutdown();
+    })
+})
---
+++ modules/plugin/src/serve.ts
@@ -0,0 +1,25 @@
+import { IResourceProviderServer, ResourceProviderService, EngineClient } from '@neoskop/pulumi-utils-grpc';
+import { Server, ServerCredentials, credentials } from 'grpc';
+import { promisify } from 'util';
+import { Writable } from 'stream';
+
+export async function serve(
+    args: string[],
+    providerFactory: (host: EngineClient) => IResourceProviderServer,
+    /* istanbul ignore next */ { stdout = process.stdout }: { stdout?: Writable | false } = {}
+) {
+    if (1 !== args.length) {
+        throw new Error('Missing argument for host RPC');
+    }
+
+    const host = new EngineClient(args[0], credentials.createInsecure());
+
+    const server = new Server();
+    server.addService(ResourceProviderService, providerFactory(host));
+
+    const port = await promisify(server.bindAsync.bind(server))('0.0.0.0:0', ServerCredentials.createInsecure());
+
+    stdout && stdout.write(`${port}\n`);
+
+    return { server, port };
+}
