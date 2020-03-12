df5fe24c3f1cd2b07e2cb0df1e5daf01c9069383
draco <wecke@neoskop.de>
2020-03-12 10:44:31 +0100

    refactor(common): move to bucket/namespace exports

--- modules/common/src/index.ts
+++ modules/common/src/index.ts
@@ -1,5 +1,4 @@
-export * from './protobuf/struct';
-export * from './utils/remove-undefined';
-export * from './utils/sleep';
-export * from './utils/tok';
-export * from './utils/uniquify';
+import * as protobuf from './protobuf/struct';
+export { protobuf };
+import * as utils from './utils';
+export { utils };
---
+++ modules/common/src/utils/index.ts
@@ -0,0 +1,4 @@
+export * from './remove-undefined';
+export * from './sleep';
+export * from './tok';
+export * from './uniquify';
