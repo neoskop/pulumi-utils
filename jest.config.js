21cdbab2e96ad32fa6bad4453eab26ad09ba2d89
draco <wecke@neoskop.de>
2020-03-12 14:41:38 +0100

    tests: ignore build files for testing

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,9 @@ module.exports = {
     reporters: ['jest-spec-reporter'],
     moduleFileExtensions: ['js', 'json', 'jsx', 'ts', 'tsx', 'node', 'd.ts'],
     testRunner: 'jest-circus/runner',
-    testPathIgnorePatterns: [ 'node_modules', '.build', 'bundles', 'dist' ],
+    testPathIgnorePatterns: [ 'node_modules', '.build', 'bundles', '/dist', 'src/grpc' ],
+    coveragePathIgnorePatterns: [ 'node_modules', '.build', 'bundles', 'dist', 'src/grpc' ],
+    modulePathIgnorePatterns: [ 'node_modules', '.build', 'bundles', 'dist', 'src/grpc' ],
     collectCoverage: true,
     coverageReporters: ["html", "json", "lcov", "text", "clover"]
 };
