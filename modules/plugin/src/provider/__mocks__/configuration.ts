35728604d616732c346eab8263fdb70a2ac819a1
draco <wecke@neoskop.de>
2020-03-16 18:14:17 +0100

    tests(plugin): fix configuration mock

--- modules/plugin/src/provider/__mocks__/configuration.ts
+++ modules/plugin/src/provider/__mocks__/configuration.ts
@@ -4,6 +4,12 @@ export class ConfigurationMock implements Configuration {
     checkConfig = jest.fn();
     diffConfig = jest.fn();
     configure = jest.fn();
+    [Symbol.iterator]() {
+        return [][Symbol.iterator]();
+    }
+    set = jest.fn();
+    get = jest.fn();
+    require = jest.fn();
 }
 
 export const ConfigurationImpl = jest.fn().mockImplementation(function() {
