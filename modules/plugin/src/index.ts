b1f8bd913d787d73347f113b7f533a4a4b6b900b
draco <wecke@neoskop.de>
2020-03-17 16:05:47 +0100

    fix(plugin): add missing exports

--- modules/plugin/src/index.ts
+++ modules/plugin/src/index.ts
@@ -1,3 +1,8 @@
 export * from '@neoskop/pulumi-utils-common';
 
 export * from './errors';
+export * from './provider';
+
+export * from './make-server';
+export * from './serve';
+export * from './types';
