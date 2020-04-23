80465085eb91d23c9222e43afd809ae85d303191
draco <wecke@neoskop.de>
2020-04-23 16:05:41 +0200

    fix(common): allow undefined values for Struct factory

--- modules/common/src/protobuf/struct.ts
+++ modules/common/src/protobuf/struct.ts
@@ -1,8 +1,10 @@
 import * as struct_pb from 'google-protobuf/google/protobuf/struct_pb';
 import { removeUndefined } from '../utils/remove-undefined';
 
+type Json = string | number | boolean | null | undefined | Json[] | { [key: string]: Json };
+
 export class Struct {
-    static fromJavaScript(value: { [key: string]: struct_pb.JavaScriptValue }) {
+    static fromJavaScript<T extends { [key: string]: Json }>(value: T) {
         return struct_pb.Struct.fromJavaScript(removeUndefined(value));
     }
 }
